7c78b7c68bb9461286914864f868c179f97412eb
fatiabdulahi <168986987+fatiabdulahi@users.noreply.github.com>
2024-08-05 07:38:34 -0700

    near to finall

--- script.js
+++ script.js
@@ -7,28 +7,28 @@ function showSlides(n) {
   let i;
   const slides = document.getElementsByClassName('mySlides');
   const dots = document.getElementsByClassName('dot');
-  if (n > slides.length) { slideIndex = 1; }    
-  if (n < 1) { slideIndex = slides.length }
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
   for (i = 0; i < slides.length; i + 1) {
     slides[i].style.display = 'none';
-}
-
-for (i = 0; i < dots.length; i + 1) {
-    dots[i].className = dots[i].className.replace('active','');
   }
-  slides[slideIndex - 1].style.display = 'block';  
+  
+for (i = 0; i < dots.length; i + 1) {
+    dots[i].className = dots[i].className.replace('active', '');
+  }
+  slides[slideIndex - 1].style.display = 'block';
   dots[slideIndex - 1].className += 'active';
 }
 
-function plusSlides (n) {
+function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-prevslid.onclick = function() {
+prevslid.onclick = function () {
   plusSlides(-1);
 };
 
-nextslid.onclick = function() {
+nextslid.onclick = function () {
   plusSlides(1);
 };
 
