7b80756d4ac1af97087093421be544d052b9b91c
fatiabdulahi <168986987+fatiabdulahi@users.noreply.github.com>
2024-08-05 06:15:46 -0700

    linter issue

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
 let slideIndex = 1;
-let prevslid = document.getElementById('prev');
-let nextslid = document.getElementById('next');
-let doted = document.getElementsByClassName('dot');
-
-showSlides(slideIndex);
+const prevslid = document.getElementById('prev');
+const nextslid = document.getElementById('next');
+const doted = document.getElementsByClassName('dot');
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-
 prevslid.onclick = plusSlides(-1);
 nextslid.onclick = plusSlides(1);
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  const slides = document.getElementsByClassName('mySlides');
+  let dots = document.getElementsByClassName('dot');
+  if (n > slides.length) { slideIndex = 1 }    
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-  slides[i].style.display = "none";  
+  slides[i].style.display = 'none';  
 }
 
 for (i = 0; i < dots.length; i++) {
-  dots[i].className = dots[i].className.replace(" active", "");
+  dots[i].className = dots[i].className.replace('active', '');
+  }
+  slides[slideIndex-1].style.display = 'block';  
+  dots[slideIndex-1].className += 'active';
 }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
+
+showSlides(slideIndex);
+
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
 
 const mySidebar = document.getElementById('mySidebar');
 
